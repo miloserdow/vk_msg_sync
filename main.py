0108dd53b054beb0ed850fbfd06bbc7e18bb3b32
Vladimir Miloserdov <milosvova@gmail.com>
2014-05-27 01:11:08 +0700

    Sorted messanges, problems with progressbar

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ maxcnt = 200 # Max messages count per request
 total_cnt = None
 token = None
 msg_dir = None
+msgs = defaultdict(list)
 
 # Get full name by id
 def get_name(id):
@@ -35,21 +36,20 @@ def msg_process(msgs):
         fname, lname = name[0], name[1]
         with open(msg_dir+'/'+fname+'_'+lname+'.msg', 'a+', encoding='utf-8') as cur_file:
             for cur_msg in msgs[id]:
-                tim = time.strftime('%Y.%m.%d %H:%M:%S', time.localtime(int(cur_msg[1])))
-                cur_file.write('['+tim+'] '+cur_msg[0]+'\n')
+                tim = time.strftime('%Y.%m.%d %H:%M:%S', time.localtime(int(cur_msg[0])))
+                cur_file.write('['+tim+'] '+(cur_msg[1] and 'me' or fname) +': '+cur_msg[2]+'\n')
 
 # This function loads messanges from the remote
-def msg_fetch(pos, cnt):
+def msg_fetch(pos, cnt, is_out):
     if token is None:
         raise RuntimeError('Token undefined: not authorized')
-    res = urllib.request.urlopen('https://api.vk.com/method/messages.get?offset='+str(pos)+'&count='
+    res = urllib.request.urlopen('https://api.vk.com/method/messages.get?out='+str(is_out)+'&offset='+str(pos)+'&count='
                                  +str(cnt)+'&access_token='+token)
     code = json.loads(res.read().decode('utf-8'))
     total_cnt = code['response'][0]
-    msgs = defaultdict(list) 
     for i in range(1, cnt + 1):
-        msgs[code['response'][i]['uid']].append((code['response'][i]['body'], code['response'][i]['date']))
-    msg_process(msgs)
+        msgs[code['response'][i]['uid']].append((code['response'][i]['date'], code['response'][i]['out'], code['response'][i]['body']))
+    #msg_process(msgs)
 
 # Program starts HERE!
 print('This program will download all your VK messages and save them into files')
@@ -110,23 +110,44 @@ with open(access_token_pth, encoding='utf-8') as token_file:
     user_id = token_file.readline().rstrip()
     print('Your id is', user_id)
 
-# Getting total count
-res = urllib.request.urlopen('https://api.vk.com/method/messages.get?offset=0&count=1&access_token='+token)
-code = json.loads(res.read().decode('utf-8'))
-remain = total_cnt = code['response'][0]
-print('Total message count:', total_cnt)
+for i in range(0, 2):    
+    res = urllib.request.urlopen('https://api.vk.com/method/messages.get?out='+str(i)+'&offset=0&count=1&access_token='+token)
+    code = json.loads(res.read().decode('utf-8'))
+    remain = total_cnt = code['response'][0]
+    print('Total ' +(i == 0 and 'inbox' or 'sent') + ' message count:', total_cnt)
+    
+    # Progress bar :)
+    bar_width = 50
+    bar_state = 0
+    part = maxcnt / total_cnt
+    step = bar_width / part
+    print(part, step)
+    sys.stdout.write("[%s]" % (" " * bar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (bar_width+1))
 
-# Fetching & processing messages
-try:
-    while remain > maxcnt:
-        msg_fetch(total_cnt - remain, maxcnt)
-        remain -= maxcnt
+    # Fetching & processing messages
+    try:
+        while remain > maxcnt:
+            msg_fetch(total_cnt - remain, maxcnt, i)
+            remain -= maxcnt
+            progress += step
+            if progress * 2 > bar_state:
+                sys.stdout.write("-" * int(step))
+                sys.stdout.flush()
+                bar_state += int(step)
+        if remain > 0:
+            msg_fetch(total_cnt - remain, remain, i)
+        sys.stdout.write("-" * ((100 - bar_state) // 2))
+        sys.stdout.write("\n")
 
-    if remain > 0:
-            msg_fetch(total_cnt - remain, remain)
-# This could happen if smth goes wrong, e.g. request failure
-except IndexError:
-    print('Error getting messages')
-    exit(1)
+    # This could happen if smth goes wrong, e.g. request failure
+    except IndexError:
+        print('Error getting messages')
+        exit(1)
 
+for x in msgs:
+    msgs[x].sort()
+msg_process(msgs)
+# TODO: Sort messages by timestamps
 print('Program has been finished successfully')
